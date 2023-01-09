b54f780b1d7cd8d30777c9d357a4039c38081d2b
Jay <chanathip2320@gmail.com>
2023-01-09 11:04:16 +0700

    uuid added

--- book-api.js
+++ book-api.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const members = require("./route/api/Members");
+const uuid = require("uuid");
 
 const app = express();
 const port = 3000;
@@ -9,31 +11,71 @@ const port = 3000;
 let books = [];
 
 app.use(cors());
+app.use(express.json());
 
 //Configuring body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false })); //handle the form submission
 app.use(bodyParser.json());
 
-
-app.get("/", (req,res) =>{
+app.get("/", (req, res) => {
   //refer to the html file and use it in /
-  res.sendFile(__dirname + '/new-book.html');
-})
+  res.sendFile(__dirname + "/new-book.html");
+});
 
+app.get("/books", (req, res) => {
+  res.json(members);
+});
 
+app.post("/book", (req, res) => {
+  const newMember = {
+    id: uuid.v4(), //this is from the npm uuid to simulate mogodb prostgruos that normally give us an id
+    name: req.body.name,
+    email: req.body.email,
+    status: "active",
+  };
 
-app.post('/book', (req, res) => {
+  if (!newMember.name || !newMember.email) {
+    return res.status(400).json({ msg: "Please include name and email" });
+  }
 
-  //add a book in the book array
-  const book = req.body;
-  //Outout the book to the console for debugging
-  console.log(book);
-  books.push(book);
-  res.send("Book is sent to the database");
+  members.push(newMember);
+  console.log(members);
+  res.json(members);
+});
 
+app.put("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    const updMember = req.body;
+    members.forEach((member) => {
+      if (member.id === parseInt(req.params.id)) {
+        member.name = updMember.name ? updMember.name : member.name;
+        member.email = updMember.email ? updMember.email : member.email;
+
+        res.json({ msg: "Member update", member });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
+app.delete("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json({
+      msg: "Member deleted",
+      members: members.filter(
+        (member) => member.id !== parseInt(req.params.id)
+      ),
+    });
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
 });
 
 app.listen(port, () =>
   console.log(`Hello world app listening on port ${port}!`)
 );
-
